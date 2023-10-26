b2303d177bb8f4e41b8abe56ee553101e0005035
dev-diegov <diegoidv.us@gmail.com>
2023-10-25 23:26:24 -0300

    get faces and draw rectangles

--- facial_recognition.py
+++ facial_recognition.py
@@ -2,19 +2,36 @@ from cv2 import cv2
 import face_recognition as fr
 
 # load images
-image_control = fr.load_image_file('images/PhotoC.jpg')
-image_test = fr.load_image_file('images/PhotoD.jpg')
-image_test2 = fr.load_image_file('images/PhotoE.jpg')
+image_control = fr.load_image_file('images/imageB.jpg')
+image_test = fr.load_image_file('images/imageC.jpg')
 
 # transform color
 image_control = cv2.cvtColor(image_control, cv2.COLOR_BGR2RGB)
 image_test = cv2.cvtColor(image_test, cv2.COLOR_BGR2RGB)
-image_test2 = cv2.cvtColor(image_test2, cv2.COLOR_BGR2RGB)
+
+# get face
+place_face_A = fr.face_locations(image_control)[0]
+codified_face_A = fr.face_encodings(image_control)[0]
+
+place_face_B = fr.face_locations(image_test)[0]
+codified_face_B = fr.face_encodings(image_test)[0]
+
+# show rectangle
+cv2.rectangle(image_control,
+              (place_face_A[3], place_face_A[0]),
+              (place_face_A[1], place_face_A[2]),
+              (0, 255, 0),
+              2)
+
+cv2.rectangle(image_test,
+              (place_face_B[3], place_face_B[0]),
+              (place_face_B[1], place_face_B[2]),
+              (0, 255, 0),
+              2)
 
 # show images
 cv2.imshow('Image 1', image_control)
 cv2.imshow('Image 2', image_test)
-cv2.imshow('Image 3', image_test2)
 
 # keep program opened
 cv2.waitKey(0)
